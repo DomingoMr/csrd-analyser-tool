130fb38839adf342551831b525409e3be0f4b8f7
Domingo <domingo.mrodenas@outlook.es>
2026-03-18 17:16:46 +0100

    dotenv arreglado

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,7 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import analysisRouter from './routes/analysis.routes.js';
-import 'dotenv/config';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
